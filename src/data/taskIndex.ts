d981b0679850a18c12ec4abcbe04ef8d60ec9ec1
Stephanie Steinshouer <stephanie@lokahiconnect.org>
2026-03-01 14:18:30 -0800

    Update taskIndex.ts

--- src/data/taskIndex.ts
+++ src/data/taskIndex.ts
@@ -4,6 +4,7 @@ import t3 from "../../data/tasks/t3_transfer_bake_ing.json";
 import t4 from "../../data/tasks/t4_matrix_jump.json";
 import t5 from "../../data/tasks/t5_matrix_make.json";
 import t6 from "../../data/tasks/t6_matrix_try.json";
+import t7 from "../../data/tasks/t7_matrix_run.json";
 
 export type Task = typeof t1;
 
@@ -13,7 +14,8 @@ export const TASKS_BY_ID: Record<string, Task> = {
   [t3.id]: t3,
   [t4.id]: t4,
   [t5.id]: t5,
-  [t6.id]: t6
+  [t6.id]: t6,
+  [t7.id]: t7
 };
 
 export function getTask(id: string): Task {
