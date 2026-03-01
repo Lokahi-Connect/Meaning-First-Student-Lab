1b0301ca0835fc293fd0a9086fdf3b2d21b2784c
Stephanie Steinshouer <stephanie@lokahiconnect.org>
2026-03-01 15:22:37 -0800

    Update taskIndex.ts
    Add t8_matrix_stop to task index

--- src/data/taskIndex.ts
+++ src/data/taskIndex.ts
@@ -5,6 +5,7 @@ import t4 from "../../data/tasks/t4_matrix_jump.json";
 import t5 from "../../data/tasks/t5_matrix_make.json";
 import t6 from "../../data/tasks/t6_matrix_try.json";
 import t7 from "../../data/tasks/t7_matrix_run.json";
+import t8 from "../../data/tasks/t8_matrix_stop.json";
 
 export type Task = typeof t1;
 
@@ -15,7 +16,8 @@ export const TASKS_BY_ID: Record<string, Task> = {
   [t4.id]: t4,
   [t5.id]: t5,
   [t6.id]: t6,
-  [t7.id]: t7
+  [t7.id]: t7,
+  [t8.id]: t8
 };
 
 export function getTask(id: string): Task {
